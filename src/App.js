b632cfc98a279c5d64469477ff731848527089cc
Mykola <odesalover@gmail.com>
2023-04-05 11:51:52 +0300

    git

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import LogIn from './components/LogIn';
+import LogOut from './components/LogOut';
+import Products from './components/Products';
+import SaveList from './components/SaveList';
 
-function App() {
+function App () {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="wrapper">
+      <div className="list">
+        <h1>Product list</h1>
+        <Products />
+        <SaveList />
+      </div>
+      <LogIn />
+      <LogOut />
+    </div> 
   );
 }
 
